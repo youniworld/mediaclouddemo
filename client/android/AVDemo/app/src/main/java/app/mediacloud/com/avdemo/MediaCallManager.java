bae79616be8226407daed0653f51d48d41f12042
youni <syyorient@qq.com>
2016-11-09 12:21:36 +0800

    update call manager

--- client/android/AVDemo/app/src/main/java/app/mediacloud/com/avdemo/MediaCallManager.java
+++ client/android/AVDemo/app/src/main/java/app/mediacloud/com/avdemo/MediaCallManager.java
@@ -1,8 +1,37 @@
 package app.mediacloud.com.avdemo;
 
+import android.content.Context;
+
 /**
  * Created by youni on 2016/11/9.
  */
 
 public class MediaCallManager {
+    public static enum CallState{
+        EConnecting("EConnecting"),
+        EAccepted("EAccepted"),
+        ERinging("ERinging"),
+        EHangup("EHangup"),
+        EReject("EReject"),
+        EInit("EInit");
+
+        CallState(String stateStr) {
+            _stateStr = stateStr;
+        }
+
+        @Override
+        public String toString() {
+            return _stateStr;
+        }
+
+        private String _stateStr;
+    }
+
+    public static interface OnCallStateChangeListener{
+        public void OnCallStateChanged(CallState state);
+    }
+
+    private Context _context;
+    private TCPClient _client;
+
 }
